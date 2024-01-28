3382b04dbc68ea8bb0b552729579d54be7387847
Rodrigo Sabino <sabinorsp@gmail.com>
2024-01-28 15:50:05 -0400

    update

--- javascript_fundamentos/fundamentos/destructuring.js
+++ javascript_fundamentos/fundamentos/destructuring.js
@@ -1,5 +1,9 @@
 // Novo recurso do ES2015
-
+/*
+Em Javascript, o desctructuring é uma técnica que permite extrair valores de arrays ou objetos
+e atribui-los a variáveis de forma conscisa. Essa técnica torna o código mais limpo 
+e legível, especialmente ao lidar com estruturas de dados complexas. 
+*/
 const pessoa = {
     nome:'Ana',
     idade: 5,
